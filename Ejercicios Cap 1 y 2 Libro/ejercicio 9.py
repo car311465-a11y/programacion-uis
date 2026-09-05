155b3ee9666c9ab87d3a5454b170e7cda650b66b
Felipe <car311465@gmail.com>
2026-09-04 19:14:55 -0500

    Fix print statement indentation and remove comments

--- Ejercicios Cap 1 y 2 Libro/ejercicio 9.py
+++ Ejercicios Cap 1 y 2 Libro/ejercicio 9.py
@@ -1,9 +1,3 @@
 nombre = input("¿Cuál es tu nombre? ")
 edad = input("¿Cuál es tu edad? ")
 print(f"Hola {nombre}, tienes {edad} años.")
-
-# Ejercicio 10
-try:
-    numero = int("Hola")
-except ValueError as e:
-    print(e)
